04be7c938491e071024f8d39a51dc83c3a8ea23e
Silviu <pascariu.octavian98@gmail.com>
2026-02-27 14:47:10 +0200

    Add demo code.

---
+++ src/app/pages/about/about.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HlmButton } from '@spartan-ng/helm/button';
+
+@Component({
+  selector: 'app-about',
+  imports: [HlmButton],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export default class About {}
